4575334c68993a54c0286b04eb9a9e934a07c4fa
ghbin <hao123guohaibin@163.com>
2015-06-19 14:08:56 +0800

    增加根视图控制器

---
+++ Test/Test/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Test
+//
+//  Created by JY on 15/6/19.
+//  Copyright (c) 2015年 jingyoutimes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
---
+++ Test/Test/RootViewController.m
@@ -0,0 +1,42 @@
+//
+//  RootViewController.m
+//  Test
+//
+//  Created by JY on 15/6/19.
+//  Copyright (c) 2015年 jingyoutimes. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    
+    // 操你妹小范不理我
+    // 操你妹小范不理我// 操你妹小范不理我
+    // 操你妹小范不理我
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
